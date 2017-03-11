50bfb5dfc17f1ecf16b68f858f0453df95c46f4a
Anya Keller <anyakeller@betelgeuse.local>
2017-03-11 15:44:11 -0500

    It sort of works? :(

--- confused.js
+++ confused.js
@@ -29,13 +29,15 @@ var makeCirc = function(x,y,r,fill,stroke){
             circ.setAttribute("stroke","black");
         };
     });
-circs.push(circ);
-return(circ);
+    circs.push(circ);
+    return(circ);
 };
 
 var move = function(){
     for (var i = 0; i < circs.length;i++){
-        dvd(circs[i]);
+        var vy = (Math.random()*2 - 1) * 4;
+        var vx = (Math.random()*2 - 1) * 4;
+        dvd(circs[i],vx,vy);
     };
 };
 
@@ -43,40 +45,72 @@ var move = function(){
 //    "http://www.w3.org/2000/svg",
 //   "circle");
 
-var dvd = function(circ){
+var dvd = function(circ,vx,vy){
     console.log("dvding");
     //init params
     var r = parseInt(circ.getAttribute("r"));
     var xcor = parseInt(circ.getAttribute("cx"));
     var ycor = parseInt(circ.getAttribute("cy"));
+    var nextxcor;
+    var nextycor;
     var fill = circ.getAttribute("fill");
     var stroke = circ.getAttribute("stroke");
-    var vy = Math.random() * 4 - 2;
-    var vx = Math.random() * 4 - 2;
     var t = 1; // time of one step
+    //invisible line
+    var centerline = width / 2;
+
     var redrawDot = function(){
         console.log("step");
         //ball
-        xcor = xcor + vx * t;
-        ycor = ycor + vy * t;
-        if (xcor > width - r || xcor < 0 + r ){
-            xcor = xcor - vx * t;
+        nextxcor = xcor + vx * t;
+        nextycor = ycor + vy * t;
+        if (nextxcor > width - r || nextxcor < 0 + r ){
             vx = -1 * vx;
-            xcor = xcor + vx * t;
+            nextxcor = xcor + vx * t;
         };
-        if (ycor > height - r || ycor < 0 + r){
-            ycor = ycor - vy * t;
+        if (nextycor > height - r || nextycor < 0 + r){
             vy = -1 * vy;
-            ycor = ycor + vy * t;
+            nextycor = ycor + vy * t;
         };
-        circ.setAttribute("cx" ,xcor);
-        circ.setAttribute("cy",ycor);
-        circ.setAttribute("r",r);
-        circ.setAttribute("fill",fill);
-        circ.setAttribute("stroke",stroke);
-        pic.appendChild(circ);
+
+        //intersection
+        if (((centerline - xcor > 0) && (centerline - nextxcor < 0)) || ((centerline - xcor < 0) && (centerline - nextxcor > 0))){
+            console.log("line passed");
+            r = r / 2;
+            if (r >= 2){
+                var newCirc = makeCirc(xcor,ycor,r,fill,stroke);
+                pic.appendChild(newCirc);
+                dvd(newCirc,vx * -1, vy * -1);
+            };
+        };
+
+        if (r >= 2) {
+            xcor = nextxcor;
+            ycor = nextycor;
+
+            circ.setAttribute("cx" ,xcor);
+            circ.setAttribute("cy",ycor);
+            circ.setAttribute("r",r);
+            circ.setAttribute("fill",fill);
+            circ.setAttribute("stroke",stroke);
+            pic.appendChild(circ);
+        };
+        if (r < 2){
+            //var ind = circs.indexOf(circ);
+            //circs.splice(ind);
+            //window.clearInterval(iid2[ind]);
+            //iid2.splice(ind);
+            var children = pic.childNodes;// != -1){
+            for (var i = 0; i > children.length; i++){
+                if (children[i] == circ){
+                    pic.removeChild(circ);
+                    break;
+                };
+            };
+        };
     };
-    iid2.push(setInterval(redrawDot,10));//interval id
+    var ind = circs.indexOf(circ);
+    iid2[ind] = setInterval(redrawDot,10);//interval id
 };
 
 var clear = function(){
